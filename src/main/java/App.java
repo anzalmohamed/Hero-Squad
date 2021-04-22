b74c1f3d580ae8b7b05117ef7d62d7aeac8d182f
anzalmohamed <anzaldidah@gmail.com>
2021-04-22 07:07:57 +0300

    added templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,22 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.Map;
 import static spark.Spark.*;
 
 public class App {
+
     public static void main(String[] args) {
-        
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> { //request for route happens at this location
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            return new ModelAndView(model, "hero.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine()); //
+
+        get("/squad_photos", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "squad.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
